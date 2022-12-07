0cfaecbe141fa811c38704529a28cade0ceb6220
l1mey112 <l-m@l-m.dev>
2022-12-08 02:47:17 +1100

    procedure calls

---
+++ lexer.v
@@ -0,0 +1,106 @@
+
+enum Tok {
+	s_if s_else s_while    // keywords
+	s_proc s_call          //
+	opar cpar obr cbr semi // punc
+	add sub mul div eq dec // ops
+	ident                  // vars
+	eof
+}
+
+struct Lexer {
+mut:
+	pos  int
+	text string
+	int_data u64
+	str_data string
+	curr Tok
+}
+
+fn is_id(ch u8) bool { return (ch >= `a` && ch <= `z`) || (ch >= `A` && ch <= `Z`) || ch == `_` }
+
+fn (mut l Lexer) get() Tok {
+	for {
+		if l.pos >= l.text.len {
+			return .eof
+		}
+
+		mut ch := l.text[l.pos] l.pos++
+
+		if ch.is_space() { continue }
+		
+		if is_id(ch) {
+			start := l.pos - 1
+			l.pos++
+			for l.pos < l.text.len {
+				c := l.text[l.pos]
+				if (c >= `a` && c <= `z`) || (c >= `A` && c <= `Z`) || (c >= `0` && c <= `9`) || c == `_` {
+					l.pos++
+					continue
+				}
+				break
+			}
+			word := l.text[start..l.pos]
+
+			match word {
+				'if'    { return .s_if    }
+				'else'  { return .s_else  }
+				'while' { return .s_while }
+				'proc'  { return .s_proc  }
+				'call'  { return .s_call  }
+				else {
+					l.str_data = word
+					return .ident
+				}
+			}
+		} else if ch.is_digit() {
+			start := l.pos - 1
+			for l.pos < l.text.len {
+				c := l.text[l.pos]
+				if !c.is_digit() {
+					if is_id(c) {
+						panic("unsuitable character in integer")
+					}
+					break
+				}
+				l.pos++
+			}
+			word := l.text[start..l.pos]
+
+			l.int_data = word.u64()
+			return .dec
+		} else {
+			match ch {
+				`{` { return .obr  }
+				`}` { return .cbr  }
+				`(` { return .opar }
+				`)` { return .cpar }
+				`+` { return .add  }
+				`-` { return .sub  }
+				`*` { return .mul  }
+				`/` { return .div  }
+				`=` { return .eq   }
+				`;` { return .semi }
+				else {}
+			}
+		}
+		
+		break
+	}
+	panic("syntax error")
+}
+
+fn (mut l Lexer) expect(i Tok) bool {
+	t := l.next()
+	return t == i
+}
+
+fn (mut l Lexer) next() Tok {
+	t := l.get()
+	l.curr = t
+	return t
+}
+
+fn (mut l Lexer) curr() Tok {
+	return l.curr
+}
---
+++ parser.v
@@ -0,0 +1,218 @@
+enum AstKind {
+	s_proc s_if s_while
+	stmt stmtseq expr empty assign
+	add sub mul div eq proc_call
+	ident dec
+}
+
+type AstValue = u64 | string
+
+struct AstNode {
+mut:
+	kind AstKind
+	value AstValue
+	n1 &AstNode = unsafe { nil }
+	n2 &AstNode = unsafe { nil }
+	n3 &AstNode = unsafe { nil }
+}
+
+struct Parser {
+mut:
+	l Lexer
+	procs []string
+}
+
+fn (mut p Parser) expr_paren() &AstNode {
+	if p.l.curr() != .opar || p.l.curr() == .eof {
+		panic("syntax error in expression")
+	}
+	p.l.next()
+	mut n := p.expr()
+	if p.l.curr() != .cpar || p.l.curr() == .eof {
+		panic("syntax error in expression")
+	}
+	p.l.next()
+	return n
+}
+
+// PRECEDENCE ident
+fn (mut p Parser) term() &AstNode {
+	match p.l.curr() {
+		.ident {
+			p.l.next()
+			return &AstNode{kind: .ident, value: p.l.str_data}
+		}
+		.dec {
+			p.l.next()
+			return &AstNode{kind: .dec, value: p.l.int_data}
+		}
+		else {
+			return p.expr_paren()
+		}
+	}
+}
+
+// PRECEDENCE '*' '/'
+fn (mut p Parser) expr2() &AstNode {
+	mut n := p.term()
+	for p.l.curr() in [.mul, .div] {
+		mut x := &AstNode{}
+		x.kind = if p.l.curr() == .mul { .mul } else { .div }
+		x.n1 = n
+		p.l.next()
+		x.n2 = p.term()
+		n = x
+	}
+	return n
+}
+
+// PRECEDENCE '+' '-'
+fn (mut p Parser) expr1() &AstNode {
+	mut n := p.expr2()
+	for p.l.curr() in [.add, .sub] {
+		mut x := &AstNode{}
+		x.kind = if p.l.curr() == .add { .add } else { .sub }
+		x.n1 = n
+		p.l.next()
+		x.n2 = p.expr2()
+		n = x
+	}
+	return n
+}
+
+// PRECEDENCE '='
+fn (mut p Parser) expr() &AstNode {
+	if p.l.curr() != .ident {
+		return p.expr1()
+	}
+	mut n := p.expr1()
+	if n.kind == .ident && p.l.curr() == .eq {
+		mut x := &AstNode{kind: .assign}
+		x.n1 = n
+		p.l.next()
+		x.n2 = p.expr()
+		n = x
+	}
+	return n
+}
+
+fn (mut p Parser) stmt() &AstNode {
+	mut n := &AstNode(0)
+
+	tok := p.l.curr()
+	match tok {
+		.s_if {
+			n = &AstNode{kind: .s_if}
+			p.l.next()
+			n.n1 = p.expr()
+			if p.l.curr() == .obr {
+				n.n2 = p.stmt()
+				if p.l.curr() == .s_else {
+					if p.l.next() == .obr {
+						n.n3 = p.stmt()
+					} else {
+						panic("expected opening brace to begin else case")
+					}
+				}
+				return n
+			}
+			panic("expected opening brace to begin if statement")
+		}
+		.s_while {
+			n = &AstNode{kind: .s_while}
+			p.l.next()
+			n.n1 = p.expr()
+			if p.l.curr() == .obr {
+				n.n2 = p.stmt()
+				p.l.next()
+				return n
+			}
+			panic("expected opening brace to begin while loop")
+		}
+		.s_call {
+			n = &AstNode{kind: .proc_call}
+			if !p.l.expect(.ident) {
+				panic("expected name of procedure to call")
+			}
+			n.n1 = &AstNode{kind: .ident, value: p.l.str_data}
+			p.l.next()
+			if p.l.curr() == .semi {
+				p.l.next()
+			} else {
+				panic("expected semicolon to complete procedure call")
+			}
+		}
+		.obr {
+			if p.l.next() != .cbr {
+				n = &AstNode{kind: .stmtseq}
+				for {
+					mut x := &AstNode{kind: .stmtseq}
+					x.n1 = n
+					x.n2 = p.stmt()
+					n = x
+					if p.l.curr() == .cbr {
+						break
+					}
+				}
+				p.l.next()
+				return n
+			} else {
+				p.l.next()
+				return &AstNode{kind: .empty}
+			}
+		}
+		.semi {
+			p.l.next()
+			n = &AstNode{kind: .empty}
+		}
+		.eof {
+			panic("unexpected eof")
+		}
+		else {
+			n = &AstNode{kind: .expr}
+			n.n1 = p.expr()
+			if p.l.curr() == .semi {
+				p.l.next()
+			} else {
+				panic("expected semicolon to complete expression")
+			}
+		}
+	}
+	return n
+}
+
+fn (mut p Parser) proc() &AstNode {
+	if p.l.curr() != .s_proc || p.l.curr() == .eof {
+		panic("procedures may only be expressed at the top level")
+	}
+	if !p.l.expect(.ident) {
+		panic("expected name of procedure")
+	}
+	if p.l.str_data in p.procs {
+		panic("duplicate function name ${p.l.str_data}")
+	}
+	p.procs << p.l.str_data
+	if !p.l.expect(.obr) {
+		panic("expected open bracket to begin procedure")
+	}
+	mut n := &AstNode{kind: .s_proc}
+	n.n1 = p.stmt()
+	return n
+}
+
+fn (mut p Parser) parse() &AstNode {
+	p.l.next()
+	mut n := &AstNode{kind: .stmtseq}
+	if p.l.curr() != .eof {
+		for {
+			mut x := &AstNode{kind: .stmtseq}
+			x.n1 = n
+			x.n2 = p.proc()
+			n = x
+			if p.l.curr() == .eof {
+				break
+			}
+		}
+	}
+	return n
+}
